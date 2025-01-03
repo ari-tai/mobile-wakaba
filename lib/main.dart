e23d807ca22ca52ebf20c759b37795550c531a58
ari-tai <arizonotaito@outlook.jp>
2025-01-03 23:57:25 +0900

    とりあえず完成

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(const MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -57,48 +58,38 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
    
     return Scaffold(
-      appBar: AppBar(
-        
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
-        title: Text(widget.title),
-      ),
+
       body: Center(
-        
-        child: Column(
-          
+        child:Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'hey guys! You have pushed the button this many times:',
+          children:[
+            Image.asset('top-images/title.png'),
+            SizedBox(
+              height:100,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            ElevatedButton(
+              onPressed: () {
+                //ボタンを押したときに実行する
+              },
+              child: Text("対戦開始"),
+              style: TextButton.styleFrom(
+                backgroundColor: Color(0xFFEC7254),
+                textStyle: const TextStyle(fontSize: 30),
+                foregroundColor: Colors.black, // foreground
+                fixedSize: Size(220, 80),
+                alignment: Alignment.center,
+              )
             ),
-          ],
-        ),
+
+           ] ,
+        ),     
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      
     );
   }
 }
